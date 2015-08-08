04e08eb7aa2a17eec700ba3fcbfc1774a160de2b
Halvard Skogsrud <halvard@skogsrud.com>
2015-08-09 00:17:38 +1000

    Show how to create routes without lambdas

--- src/main/java/com/skogsrud/halvard/sparkjava/spike/HelloWorld.java
+++ src/main/java/com/skogsrud/halvard/sparkjava/spike/HelloWorld.java
@@ -1,9 +1,20 @@
 package com.skogsrud.halvard.sparkjava.spike;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
         get("/hello", (request, response) -> "Hello World");
+
+        get("/hello7", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return "Hello World without lambda";
+            }
+        });
     }
 }
