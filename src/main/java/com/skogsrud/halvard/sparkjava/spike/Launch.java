bd28156d296adf44d5e498ebc72e7a6293680514
Halvard Skogsrud <halvard@skogsrud.com>
2015-08-11 16:40:45 +1000

    Read port from environment variable or system property

--- src/main/java/com/skogsrud/halvard/sparkjava/spike/Launch.java
+++ src/main/java/com/skogsrud/halvard/sparkjava/spike/Launch.java
@@ -1,6 +1,7 @@
 package com.skogsrud.halvard.sparkjava.spike;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import spark.SparkBase;
 
 import static spark.Spark.before;
 import static spark.SparkBase.port;
@@ -11,7 +12,7 @@ public class Launch {
     }
 
     public void run() throws Exception {
-        port(4567);
+        port(getPort());
 
         ObjectMapper objectMapper = new ObjectMapper();
 
@@ -20,6 +21,21 @@ public class Launch {
         new Image().run();
 
         disableCaching();
+    }
+
+    /**
+     * Use port from 'PORT' environment variable or 'port' system property, default is 4567.
+     * Specify port 0 for a randomly assigned port.
+     */
+    private int getPort() {
+        int port = SparkBase.SPARK_DEFAULT_PORT; // 4567
+        if (System.getProperty("port") != null) {
+            port = Integer.getInteger(System.getProperty("port"));
+        }
+        if (System.getenv("PORT") != null) {
+            port = Integer.getInteger(System.getenv("PORT"));
+        }
+        return port;
     }
 
     /**
