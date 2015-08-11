7ef89bf226fa1ec13f29dcb201752ac9b668dd83
Halvard Skogsrud <halvard@skogsrud.com>
2015-08-11 16:40:23 +1000

    Allow request bodies either as JSON or form encoded

--- src/main/java/com/skogsrud/halvard/sparkjava/spike/Books.java
+++ src/main/java/com/skogsrud/halvard/sparkjava/spike/Books.java
@@ -1,16 +1,15 @@
 package com.skogsrud.halvard.sparkjava.spike;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import spark.Request;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import static spark.Spark.*;
 
-/**
- * Adapted from https://github.com/perwendel/spark
- */
 public class Books {
     private static Map<String, Book> books = new HashMap<>();
     private final ObjectMapper objectMapper;
@@ -20,10 +19,12 @@ public class Books {
     }
 
     public void run() throws Exception {
-        // Creates a new book resource, will return the ID to the created resource
-        // author and title are sent as JSON, e.g., { "author": "Foo", "title": "Bar" }
-        post("/books", "application/json", (request, response) -> {
-            Book book = objectMapper.readValue(request.body(), Book.class);
+        // Creates a new book resource, will return the id of the created resource.
+        // author and title should be sent either:
+        // 1. as JSON, e.g., { "author": "Foo", "title": "Bar" } ; or
+        // 2. as form-encoded values in the request body, e.g., author=Foo&title=Bar
+        post("/books", (request, response) -> {
+            Book book = readBookFromRequestBody(request);
             String id = UUID.randomUUID().toString();
             books.put(id, book);
             response.status(201);
@@ -43,10 +44,10 @@ public class Books {
         }, objectMapper::writeValueAsString);
 
         // Updates the book resource for the provided id with new information
-        // author and title are sent as query parameters e.g. /books/<id>?author=Foo&title=Bar
+        // author and title should be sent as per the create 'post' route
         put("/books/:id", (request, response) -> {
             String id = request.params(":id");
-            Book book = new Book(request.queryParams("author"), request.queryParams("title"));
+            Book book = readBookFromRequestBody(request);
             if (books.get(id) != null) {
                 books.put(id, book);
                 return book;
@@ -72,6 +73,21 @@ public class Books {
             response.type("application/json");
             return books;
         }, objectMapper::writeValueAsString);
+    }
+
+    private Book readBookFromRequestBody(Request request) throws IOException {
+        Book book = null;
+        switch (request.contentType()) {
+            case "application/json":
+                book = objectMapper.readValue(request.body(), Book.class);
+                break;
+            case "application/x-www-form-urlencoded":
+                book = new Book(request.queryParams("author"), request.queryParams("title"));
+                break;
+            default:
+                halt(400, "Unsupported request content-type [" + request.contentType() + "]");
+        }
+        return book;
     }
 
     /**
